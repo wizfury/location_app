a19ea341bf36f7100dde0f162fdea28b61215838
wizfury <Aakarsh135@gmail.com>
2023-05-29 02:10:44 +0530

    changing state

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,9 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
 
